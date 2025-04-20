eca6b3a0a31ff043e6263823aede5fbeb12ede8f
jelez25 <elez250199@gmail.com>
2025-04-20 19:05:35 +0200

    Add name and surname fields to CustomUser model and update forms and profile template

--- easy_sheets/accounts/forms.py
+++ easy_sheets/accounts/forms.py
@@ -5,7 +5,18 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = ['username', 'role','email', 'birth_date', 'password1', 'password2']
+        fields = ['name', 'surname_1', 'surname_2', 'username', 'role', 'email', 'birth_date', 'password1', 'password2']
+        labels = {
+            'name': 'Nombre',
+            'surname_1': 'Primer Apellido',
+            'surname_2': 'Segundo Apellido',
+            'username': 'Nombre de Usuario',
+            'role': 'Rol',
+            'email': 'Correo Electrónico',
+            'birth_date': 'Fecha de Nacimiento',
+            'password1': 'Contraseña',
+            'password2': 'Confirmar Contraseña',
+        }
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
--- easy_sheets/accounts/models.py
+++ easy_sheets/accounts/models.py
@@ -10,6 +10,9 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     birth_date = models.DateField(null=True, blank=True)  # Birth date
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')  # Role field
+    name = models.CharField(max_length=150)  # First name
+    surname_1 = models.CharField(max_length=150)  # First surname
+    surname_2 = models.CharField(max_length=150, blank=True)  # Second surname
 
     def __str__(self):
-         return f"{self.username} ({self.role})"
+        return f"{self.username} ({self.role})"
